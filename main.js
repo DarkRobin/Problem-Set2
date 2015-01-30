46e1f50d5f3e91ea2d9dfe5d4216a35298d58790
DarkRobin <hulk@Hulk.local>
2015-01-29 20:12:27 -0700

    final commit

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
-$(document).on('ready', function() {
-  
-});
+var firstReverse = function(str){
+	return str.reverse();
+};
+
+
+console.log(firstReverse(['one', 'two', 'three', 'four', 'five']));
+console.log(firstReverse(['way', 'this', 'go', 'can', 'words']));
+
+
+
+
+var swapCase = function(swaping){
+	var temp = "";
+
+	for ( var i = 0; i < swaping.length; i++){
+
+		var letter = swaping[i];
+
+		if (letter === letter.toUpperCase()){
+			// += is short hand for temp = temp + swaping[i].toLowerCase()
+			temp += letter.toLowerCase();
+		}
+		else {
+			temp += letter.toUpperCase();
+		}
+		
+	}
+	
+	return temp;
+};
+
+
+console.log(swapCase("Hello World"));
+
+
+
+
+
+
+
+
+
+
+
+
